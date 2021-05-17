ca45c0af5db5582656d0f1803ef3213ab6d8793d
Brian Moon <apollomoon@knights.ucf.edu>
2021-05-17 00:17:38 -0400

    added background image, stock information, and added stock widgets to homepage and stock news page

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 // Stock Market Search App by Brian Moon
 
 // creating an express application in our js app
+const { object } = require("assert-plus");
 const express = require("express"); // pulls express into our app
 const app = express(); // allows us to use express
 // add a reference to handlevars
 const exphbs = require("express-handlebars");
 // we need to add path to app
 const path = require("path");
+// request for api
+const request = require("request");
 
 // telling app which port to listen on; using OR for webhosts that are using their own port (we're using 5000)
 const PORT = process.env.PORT || 5000;
+
+// API Key: pk_acb37dd339c446f2b589742ab463a39a
+function call_api(requestcompleted) {
+  request(
+    "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_acb37dd339c446f2b589742ab463a39a",
+    { json: true },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      if (res.statusCode === 200) {
+        requestcompleted(body);
+      }
+    }
+  );
+}
+// MY SIMPLE API CALL
+// var returned_request = request(
+//   "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_acb37dd339c446f2b589742ab463a39a",
+//   { json: true }
+// );
+// error handling version of api request
+// request(
+//   "https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_acb37dd339c446f2b589742ab463a39a",
+//   { json: true },
+//   (err, res, body) => {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     if (res.statusCode === 200) {
+//       console.log(body);
+//     }
+//   }
+// );
 
 // Set handlebars middleware
 app.engine("handlebars", exphbs());
@@ -17,8 +54,14 @@ app.set("view engine", "handlebars");
 
 // Set handlebar routes
 // route to homepage
+// app.get("/", function (req, res) {
+//   res.render("home", { stock_call: returned_request.symbol });
+// });
+// for function version of api call
 app.get("/", function (req, res) {
-  res.render("home", { factorial: "Factorial of 10 is: " + factorial(10) });
+  call_api(function (request) {
+    res.render("home", { stock_call: request });
+  });
 });
 
 // routing to second dynamic page
